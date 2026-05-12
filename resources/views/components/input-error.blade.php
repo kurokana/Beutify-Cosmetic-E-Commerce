fb9d496b888cdf0a179d11cb1a9c59c76bb509b4
kurokana <kunhide568@gmail.com>
2026-05-12 14:26:35 +0700

    fix input error product admin

--- resources/views/components/input-error.blade.php
+++ resources/views/components/input-error.blade.php
@@ -1,9 +1,14 @@
 @props(['messages'])
 
 @if ($messages)
+    @php
+        $normalizedMessages = collect((array) $messages)
+            ->flatten()
+            ->filter(fn ($message) => $message !== null && $message !== '');
+    @endphp
     <ul {{ $attributes->merge(['class' => 'text-sm text-red-500 space-y-1 font-medium']) }}>
-        @foreach ((array) $messages as $message)
-            <li>{{ $message }}</li>
+        @foreach ($normalizedMessages as $message)
+            <li>{{ is_string($message) ? $message : json_encode($message) }}</li>
         @endforeach
     </ul>
 @endif
